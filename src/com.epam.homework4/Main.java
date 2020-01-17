1da0a62dc183da7467cc709ad27be21f4c530843
Ilya <starodubtcev1909@gmail.com>
2020-01-17 02:51:17 +0200

    4rd homework 1 task

---
+++ src/com.epam.homework4/Main.java
@@ -0,0 +1,10 @@
+package com.epam.homework4;
+
+public class Main {
+    public static void main(String[] args) {
+        UniqueCharacters uniqueCharacters = new UniqueCharacters();
+        uniqueCharacters.process();
+//      input string  "Geeks for Geeks" for program performance check
+
+    }
+}
