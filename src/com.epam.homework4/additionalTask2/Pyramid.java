c6765e31284376cd4974fae12060a5d5573462de
Ilya <starodubtcev1909@gmail.com>
2020-01-19 20:16:43 +0200

    4th homework 2nd additional task RETURNS ONLY LEFT TRIANGLE NOT PYRAMID

--- src/com.epam.homework4/additionalTask2/Pyramid.java
+++ src/com.epam.homework4/additionalTask2/Pyramid.java
@@ -7,30 +7,41 @@ public class Pyramid {
 
     public void process() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter height of pyramid:");
         setHeight(scanner.nextInt());
+        System.out.println("The result is:");
         System.out.println(getPyramid());
     }
 
     private String getPyramid() {
-        String pyramid;
-//        if (getHeight() == 1) {
-//            pyramid = "1";
-//        }
-        int i = 0;
-        while (i < getHeight()){
-            while (i < 2 * getHeight() - 1){
-                
-            }
-        }
+        String pyramid = "";
 
-        return "";
+        int i = 0;
+        int height = 1;
+
+        while (i < getHeight()) {
+            int j = getHeight();
+            while (j > i + 1) {
+                pyramid += " ";
+                j--;
+            }
+            j = i;
+            while (j >= 0) {
+                pyramid += height++;
+                j--;
+            }
+            height = 1;
+            i++;
+            pyramid += "\n";
+        }
+        return pyramid;
     }
 
-    public int getHeight() {
+    private int getHeight() {
         return height;
     }
 
-    public void setHeight(int height) {
+    private void setHeight(int height) {
         this.height = height;
     }
 }
